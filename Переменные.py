90e550737a913fa35fd403e5bf52323cc3acf9ac
Vladislav36366 <mihailo04@mail.ru>
2024-08-23 12:35:22 +0500

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,7 @@
+number_of_homework= 12
+number_of_spent_hours= 1.5
+Course_name= "Python"
+Time_for_one_task = number_of_spent_hours / number_of_homework
+print( "Курс:", str(Course_name)+',', 'всего зачач:', str(number_of_homework)+',', 'затраченно часов', str(float(number_of_spent_hours))+',','среднее время выплонения', float(Time_for_one_task), 'часа')
+#str(float(number_of_spent_hours))+',' есть другой вариант как поставить без пробела , после float(number_of_spent_hours)?
+
